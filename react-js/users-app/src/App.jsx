98637fb5168c750c4e6caab2f36d6d58ec3e8099
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2025-09-16 08:02:58 +0530

    React JS Day-1

---
+++ react-js/users-app/src/App.jsx
@@ -0,0 +1,36 @@
+// class
+// 1. this keyword
+// 2. in-build state
+// 3. follow all the rules
+// 4. render method => html like code called as JSX i.e JavaScript XML
+// function
+
+import "./App.css";
+import UserForm from "./UserForm";
+
+export default function App() {
+  let text = "Deepakkumar";
+  return (
+    <>
+      <div className="div">Hello</div>
+      <div
+        style={{
+          color: "pink",
+          backgroundColor: "green",
+        }}
+      >
+        Hello Its React JS {text}
+      </div>
+      <UserForm />
+      <UserForm />
+      <UserForm />
+      <UserForm />
+    </>
+  );
+}
+
+// class => className
+// for => htmlFor
+// open => close it
+// only parent i.e element or <> (React Fragment)
+// javascript code in jsx => {}
---
+++ react-js/users-app/src/UserForm.jsx
@@ -0,0 +1,9 @@
+function UserForm() {
+  return (
+    <>
+      <h2>User Form</h2>
+    </>
+  );
+}
+
+export default UserForm;
